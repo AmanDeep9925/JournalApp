fdcb34348ba530ee0b9a35c146ed71eaf4045d5c
Aman Deep <amandeep58257@gmail.com>
2024-12-14 23:43:20 +0530

    Initial Commit
    Basic crud application for journal app

---
+++ src/test/java/com/bujo/journalApp/JournalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bujo.journalApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
