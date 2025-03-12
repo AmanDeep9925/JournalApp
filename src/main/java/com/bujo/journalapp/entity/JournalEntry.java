7590d427b1a8397106b601341cc79a7630701719
Aman Deep <amandeep58257@gmail.com>
2025-03-12 23:12:15 +0530

    Lombok
    Added lombok dependency to project

--- src/main/java/com/bujo/journalapp/entity/JournalEntry.java
+++ src/main/java/com/bujo/journalapp/entity/JournalEntry.java
@@ -1,6 +1,9 @@
 package com.bujo.journalapp.entity;
 
-import org.bson.types.ObjectId;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
@@ -8,6 +11,10 @@ import org.springframework.data.mongodb.core.mapping.MongoId;
 import java.time.LocalDateTime;
 
 @Document(collection = "journal_entries")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class JournalEntry {
     @Id
     @MongoId
@@ -15,36 +22,4 @@ public class JournalEntry {
     private String title;
     private String content;
     private LocalDateTime createdAt;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
 }
